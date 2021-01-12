40cea88f551025477d308d5b675a2eee0b1d3817
Akila Mendis <akila@oneit.com.au>
2021-01-12 09:23:20 +0530

    089 Assembling a Navigator

---
+++ src/screens/SearchScreen.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const SearchScreen = () => {
+
+    return (
+        <View>
+            <Text>Hello from SearchScreen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
