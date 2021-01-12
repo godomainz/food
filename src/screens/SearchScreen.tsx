be4d72c8857025d555ce700296167641ac17c203
Akila Mendis <akila@oneit.com.au>
2021-01-12 12:06:29 +0530

    099 Error Handling

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -6,21 +6,27 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
     const [term, setTerm] = useState<string>('');
     const [results, setResults] = useState<[]>([]);
+    const [errorMessage, setErrorMessage] = useState<string>('');
 
     const searchApi = async() => {
-        const response = await yelp.get('/search', {
-            params: {
-                limit: 50,
-                term,
-                location: 'san jose'
-            }
-        });
-        setResults(response.data.businesses)
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    limit: 50,
+                    term,
+                    location: 'san jose'
+                }
+            });
+            setResults(response.data.businesses)
+        }catch (e){
+            setErrorMessage('Something went wrong');
+        }
     }
 
     return (
         <View>
             <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi}/>
+            {errorMessage ? <Text>{errorMessage}</Text> : null}
             <Text>We have found {results.length} results</Text>
         </View>
     );
